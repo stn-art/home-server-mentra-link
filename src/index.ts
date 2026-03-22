db384555a2150f37d16ca04b75c87ac08bdc7682
stn-art <stn-art@email.com>
2026-03-23 01:37:26 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -5,55 +5,6 @@ const PACKAGE_NAME = process.env.PACKAGE_NAME ?? (() => { throw new Error('PACKA
 const MENTRAOS_API_KEY = process.env.MENTRAOS_API_KEY ?? (() => { throw new Error('MENTRAOS_API_KEY is not set'); })();
 const PORT = parseInt(process.env.PORT) ?? (() => { throw new Error('PORT is not set'); })();
 
-class Bridge extends AppServer {
-
-  constructor() {
-    super({
-      packageName: PACKAGE_NAME,
-      apiKey: MENTRAOS_API_KEY,
-      port: PORT,
-    });
-  }
-
-  protected async onSession(session: AppSession, sessionId: string, userId: string): Promise<void> {
-    // Show welcome message
-    session.layouts.showTextWall("Example App is ready!");
-    session.layouts.showDoubleTextWall(
-      'Temperature: 72°F',
-      'Humidity: 45%'
-    );
-    // Handle real-time transcription
-    // requires microphone permission to be set in the developer console
-    session.events.onTranscription(async (data) => {
-      if (data.isFinal) {
-        session.layouts.showTextWall("You said: " + data.text, {
-          view: ViewType.MAIN,
-          durationMs: 5000
-        });
-        await renderTextToBitmap("Масло масляное, маслом масляным маслено намазано", session);
-      }
-      else {
-        session.layouts.showTextWall("Transcribing: " + data.text, {
-          view: ViewType.MAIN
-        });
-      }
-      session.layouts.showTextWall('Что ты выбираешь, когда хочешь сладкого?\n|> ✅Да <|\n\|  ⚠️Нет  |');
-    })
-
-    session.events.onButtonPress(data => {
-      session.dashboard.content.write('✅ Task completed');
-      if (data.pressType === 'long') {
-        session.layouts.showTextWall('Что ты выбираешь, когда хочешь сладкого?\n|> ✅Да <|\n\|  ⚠️Нет  |');
-      }
-      else {
-        session.layouts.showTextWall('Что ты выбираешь, когда хочешь сладкого?\n|  Да  |\n\|> Нет <|');
-      }
-    })
-  }
-}
-
-
-
 async function renderText(session, text) {
   const width = 640;
   const height = 200;
@@ -108,6 +59,56 @@ async function renderText(session, text) {
     bitmap
   });
 }
+
+class Bridge extends AppServer {
+
+  constructor() {
+    super({
+      packageName: PACKAGE_NAME,
+      apiKey: MENTRAOS_API_KEY,
+      port: PORT,
+    });
+  }
+
+  protected async onSession(session: AppSession, sessionId: string, userId: string): Promise<void> {
+    // Show welcome message
+    session.layouts.showTextWall("Example App is ready!");
+    session.layouts.showDoubleTextWall(
+      'Temperature: 72°F',
+      'Humidity: 45%'
+    );
+    // Handle real-time transcription
+    // requires microphone permission to be set in the developer console
+    session.events.onTranscription(async (data) => {
+      if (data.isFinal) {
+        session.layouts.showTextWall("You said: " + data.text, {
+          view: ViewType.MAIN,
+          durationMs: 5000
+        });
+        await renderTextToBitmap("Масло масляное, маслом масляным маслено намазано", session);
+      }
+      else {
+        session.layouts.showTextWall("Transcribing: " + data.text, {
+          view: ViewType.MAIN
+        });
+      }
+      session.layouts.showTextWall('Что ты выбираешь, когда хочешь сладкого?\n|> ✅Да <|\n\|  ⚠️Нет  |');
+    })
+
+    session.events.onButtonPress(data => {
+      session.dashboard.content.write('✅ Task completed');
+      if (data.pressType === 'long') {
+        session.layouts.showTextWall('Что ты выбираешь, когда хочешь сладкого?\n|> ✅Да <|\n\|  ⚠️Нет  |');
+      }
+      else {
+        session.layouts.showTextWall('Что ты выбираешь, когда хочешь сладкого?\n|  Да  |\n\|> Нет <|');
+      }
+    })
+  }
+}
+
+
+
 
 const app = new Bridge();
 app.start().catch(console.error);
